7ce7b4803e32100f4a6406431f2fa8f31711a030
Jacob <kungpaojake@gmail.com>
2014-11-21 13:04:58 -0600

    Entering database creation function in commandHandler

--- PharmacyManager/src/dataLayer/DBTest.java
+++ PharmacyManager/src/dataLayer/DBTest.java
@@ -4,6 +4,13 @@ import java.sql.*;
 public class DBTest {
 	 public static void main( String args[] )
 	  {
+		 //commandHandler.create();
+		 create();
+		 insert();
+		 select();
+		 update();
+		 select();
+		 delete();
 		 select();
 	  }
 	 ///// Functions below-
@@ -18,7 +25,7 @@ public class DBTest {
 		      stmt = c.createStatement();
 		      String sql = "CREATE TABLE COMPANY " +
 		                   "(ID INT PRIMARY KEY     NOT NULL," +
-		                   " NAME           TEXT    NOT NULL, " + 
+		                   " NAME           VARCHAR    NOT NULL, " + 
 		                   " AGE            INT     NOT NULL, " + 
 		                   " ADDRESS        CHAR(50), " + 
 		                   " SALARY         REAL)"; 
@@ -141,5 +148,42 @@ public class DBTest {
 		    }
 		    System.out.println("Operation done successfully");	 
 	 }
-	 
+	 public static void delete()
+	  {
+	    Connection c = null;
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	      c.setAutoCommit(false);
+	      System.out.println("Opened database successfully");
+
+	      stmt = c.createStatement();
+	      String sql = "DELETE from COMPANY where ID=2;";
+	      stmt.executeUpdate(sql);
+	      c.commit();
+
+	      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+	      while ( rs.next() ) {
+	         int id = rs.getInt("id");
+	         String  name = rs.getString("name");
+	         int age  = rs.getInt("age");
+	         String  address = rs.getString("address");
+	         float salary = rs.getFloat("salary");
+	         System.out.println( "ID = " + id );
+	         System.out.println( "NAME = " + name );
+	         System.out.println( "AGE = " + age );
+	         System.out.println( "ADDRESS = " + address );
+	         System.out.println( "SALARY = " + salary );
+	         System.out.println();
+	      }
+	      rs.close();
+	      stmt.close();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Operation done successfully");
+	  }
 }
--- PharmacyManager/src/dataLayer/commandHandler.java
+++ PharmacyManager/src/dataLayer/commandHandler.java
@@ -1,6 +1,51 @@
 package dataLayer;
+import java.sql.*;
 
 public class commandHandler {
 	
+	/*  // exp date perhaps?...I'll put it in later -j
+	*--------------------------medicine--------------------------
+	*|  medicineID  |  name  |  lowThreshold  |  overThreshold  |  
+	*|----------------------------------------------------------|
+	*|     Int      |varchar |      Int       |       Int       |
+	*|----------------------------------------------------------|
+	*|      0       |'asprin'|       3        |       300       |
+	*|----------------------------------------------------------|
+	*|     ...      |  ...   |      ...       |       ...       |
+	*------------------------------------------------------------
+	*/
+	
+	 // create is called once at the beginning to create database + tables
+	 public static void create(){
+		 Connection c = null;
+		    Statement stmt = null;
+		    try {
+		      Class.forName("org.sqlite.JDBC");
+		      c = DriverManager.getConnection("jdbc:sqlite:pharmacy.db");
+		      
+		      stmt = c.createStatement();
+		      String sql = 	"CREATE TABLE medicine " +
+		                   	"(medicineID 	INT PRIMARY KEY     NOT NULL," +
+		                   	" name          VARCHAR				NOT NULL, " + 
+		                   	" lowThreshold	INT     			NOT NULL, " + 
+		                   	" overThreshold	INT					NOT NULL)";
+		      stmt.executeUpdate(sql);
+		    /* If another table is required in database, use this
+		      sql = 		"CREATE TABLE medicine " +
+	                   		"(medicineID 	INT PRIMARY KEY     NOT NULL," +
+	                   		" name          VARCHAR    NOT NULL, " + 
+	                   		" lowThreshold	INT     NOT NULL, " + 
+	                   		" overThreshold	INT		NOT NULL)";
+		      stmt.executeUpdate(sql);
+	      */
+		      stmt.close();
+		      c.close();
+		      
+		    } catch ( Exception e ) {
+		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		      System.exit(0);		     
+		    }
+		    System.out.println("Opened database successfully");	 
+	 }
 
 }
